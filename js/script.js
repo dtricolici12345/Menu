2dfc2ac556a6377a781894ca805581b1afaa812e
Daniela <dtricolici12345@gmail.com>
2024-06-30 10:45:26 +0200

    added class MenuCard

--- js/script.js
+++ js/script.js
@@ -105,39 +105,119 @@ window.addEventListener("DOMContentLoaded", () => {
     modal = document.querySelector(".modal"),
     modalCloseBtn = document.querySelector("[data-close]");
 
+  function openModal() {
+    modal.classList.toggle("show");
+    // modal.classList.add("show"), modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
   modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.classList.toggle("show");
-      // modal.classList.add("show"), modal.classList.remove("hide");
-      document.body.style.overflow = "hidden";
-    });
+    btn.addEventListener("click", openModal);
   });
 
-function closeModal() {
-   // modal.classList.add("hide"), modal.classList.remove("show");
-   modal.classList.toggle("show");
-   document.body.style.overflow = "";
-}
+  function closeModal() {
+    // modal.classList.add("hide"), modal.classList.remove("show");
+    modal.classList.toggle("show");
+    document.body.style.overflow = "";
+  }
 
+  modalCloseBtn.addEventListener("click", closeModal);
 
-  modalCloseBtn.addEventListener("click", closeModal)
- 
-
-  modal.addEventListener('click', (e) => {
+  modal.addEventListener("click", (e) => {
     if (e.target === modal) {
-      closeModal()
+      closeModal();
     }
   });
 
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Espace" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
 
+  //Modal ending
+  // const modalTimerId = setTimeout(openModal, 5000);
 
-document.addEventListener('keydown', (e) => {
-
-  if (e.code === "Espace" && modal.classList.contains('show')) {
-    closeModal()
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
   }
-})
+
+  window.addEventListener("scroll", showModalByScroll);
+
+  // Class for cards
+
+  class MenuCard {
+    constructor(src, alt, title, description, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.description = description;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changetoUAH();
+    }
+    changetoUAH() {
+      this.price = +this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+  <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.description}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+`;
+      this.parent.append(element);
+    }
+  }
+
+ new MenuCard(
+  "img/tabs/vegy.jpg",
+  "vegy",
+ ' Меню "Фитнес"', 
+' Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
+33, 
+'.menu .container'
+ ).render();
+
+ new MenuCard(
+  "img/tabs/elite.jpg",
+  "elite",
+ ' Меню “Премиум”', 
+' В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
+56, 
+'.menu .container'
+ ).render();
+
+ new MenuCard(
+  "img/tabs/post.jpg",
+ "post",
+ ' Меню "Постное"', 
+'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 
+16, 
+'.menu .container'
+ ).render();
 
 
 
-});
+
+}); 
+
+
+
+
